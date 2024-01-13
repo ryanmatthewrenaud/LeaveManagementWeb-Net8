f7583624e62831d2e36d28a89dfec29046d75202
ryanmatthewrenaud <ryanmatthewrenaud@gmail.com>
2024-01-13 12:09:19 -0500

    Added several migrations and began work on custom databases.

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation
+    {
+        public int Id { get; set; }
+
+        public int NumberOfDays { get; set; }
+
+        // Foreign key relationship
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId {  get; set; }
+
+        public string EmployeeID { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+    }
+}
